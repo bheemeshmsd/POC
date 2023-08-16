bca0859b0a5bda7a3a3ba907324867978144ab3b
Bheemesh Balamurugan <bheemeshbalamurugan@Bheemeshs-MacBook-Pro.local>
2023-08-16 11:06:32 +0530

    codeclean up

--- src/App.js
+++ src/App.js
@@ -24,20 +24,8 @@ const App = () => {
     console.log(tab);
   };
 
-  const handleModal = () => {
-    setModal((modalActive) => true);
-  };
 
-  const handleClose = () => {
-    setModal(false);
-    setCurrentItem("");
-  };
-
-  const handleInputSubmit = () => {
-    dispatch(addItem({ value: currentItem, checkbox: false }));
-    setModal(false);
-    setCurrentItem("");
-  };
+ 
 
   const handleTodo = () => {
     setToDoListCount((todoListCount) => todoListCount + 1);
@@ -48,22 +36,21 @@ const App = () => {
     dispatch(deleteArray({ index: index }));
     setToDoListCount((todoListCount) => todoListCount - 1);
   };
-  const [modalActive, setModal] = useState(false);
-  const [currentItem, setCurrentItem] = useState("");
+
   const [todoListCount, setToDoListCount] = useState(1);
   let listItemArray = state.list;
 
   console.log(listItemArray);
 
   //Automatic detetion will attendend this later
-  useEffect(() => {
-    // state.list.map((value, index) => {
-    //   console.log(index, value.length);
-    //   if (value?.length == 0 && index >0) {
-    //     dispatch(deleteArray(index));
-    //   }
-    // });
-  }, [state.list]);
+//   useEffect(() => {
+//     // state.list.map((value, index) => {
+//     //   console.log(index, value.length);
+//     //   if (value?.length == 0 && index >0) {
+//     //     dispatch(deleteArray(index));
+//     //   }
+//     // });
+//   }, [state.list]);
 
   return (
     <div className="containerWapper">
@@ -111,16 +98,7 @@ const App = () => {
         </div>
       </div>
 
-      {/* <div className={modalActive ? "modalContainer" : "modalContainerRemove"}>
-            <section className="modalContent">
-                <div className="modalNavBar"><button onClick={handleClose}>Close</button></div>
 
-                <div className="modalBody">
-                    <input value={currentItem} onChange={handleListImput}></input>
-                    <button onClick={handleInputSubmit}>Submit</button>
-                </div>
-            </section>
-        </div> */}
     </div>
   );
 };
