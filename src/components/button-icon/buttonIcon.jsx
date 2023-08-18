f7d584e8d003ef59dcdd15c52d6540167da5143d
bheemeshmsd <bheemesh@madstreetden.com>
2023-08-18 16:10:47 +0530

    Nav bar completed

--- src/components/button-icon/buttonIcon.jsx
+++ src/components/button-icon/buttonIcon.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const ButtonIcon = ({ iconUrl }) => {
+const ButtonIcon = ({ iconUrl, height, width ,padding}) => {
   return (
-    <button className="buttonContainer" >
+    <button
+      className="buttonContainer"
+      style={{ height: height || "40px", width: width || "40px", padding: padding }}
+    >
       <img src={iconUrl}></img>
     </button>
   );
--- src/components/navbar/navBar.jsx
+++ src/components/navbar/navBar.jsx
@@ -1,33 +1,65 @@
 import React from "react";
 import Remove from "../../../assests/icons/remove.png";
+import BurgerMenu from "../../../assests/icons/burger-bar.png";
+import Keeps from "../../../assests/icons/keeps.png";
+import Refresh from "../../../assests/icons/refresh.png";
+import Grid from "../../../assests/icons/visualization.png";
+import Settings from "../../../assests/icons/settings.png";
+import Search from "../../../assests/icons/search.png";
+import Menu from "../../../assests/icons/menu.png";
+
 import ButtonIcon from "../button-icon";
 
 const NavBar = () => {
   return (
     <nav className="navbarContainer">
       <section className="left">
-        <ButtonIcon iconUrl={Remove} />
-        <div className="iconTab">
-          <ButtonIcon iconUrl={Remove} />
-          <h3>Keep</h3>
+        <ButtonIcon
+          iconUrl={BurgerMenu}
+          height={"35px"}
+          width={"35px"}
+          padding={"7px"}
+        />
+        <div className="iconTab prevent-hover">
+          <ButtonIcon iconUrl={Keeps}  width={"50px"} height={"50px"} />
+          <p>Keep</p>
         </div>
       </section>
       <section className="middle">
         <div className="inputWrapper">
-        <ButtonIcon iconUrl={Remove} />
-        <input />
+          <ButtonIcon iconUrl={Search} padding={"10px"} />
+          <input placeholder="Search"/>
         </div>
-       
       </section>
       <section className="right">
         <div className="rightGrp1">
-          <ButtonIcon iconUrl={Remove} />
-          <ButtonIcon iconUrl={Remove} />
-          <ButtonIcon iconUrl={Remove} />
+          <ButtonIcon
+            iconUrl={Refresh}
+            width={"42px"}
+            height={"42px"}
+            padding={"1opx"}
+          />
+          <ButtonIcon
+            iconUrl={Grid}
+            width={"42px"}
+            height={"42px"}
+            padding={"10px"}
+          />
+          <ButtonIcon
+            iconUrl={Settings}
+            width={"42px"}
+            height={"42px"}
+            padding={"10px"}
+          />
         </div>
         <div className="rightGrp2">
-          <ButtonIcon iconUrl={Remove} />
-          <ButtonIcon iconUrl={Remove} />
+          <ButtonIcon
+            iconUrl={Menu}
+            width={"42px"}
+            height={"42px"}
+            padding={"12px"}
+          />
+          <ButtonIcon iconUrl={Remove} width={"45px"} height={"45px"} />
         </div>
       </section>
     </nav>
